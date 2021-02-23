b8a9e71a7071ee61a22e7a139f9231da62ad6960
Allyson <allysoneduardo04@gmail.com>
2021-02-23 18:03:17 -0300

    feat: added function to remove notes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 const GetHelp = document.querySelector(".help-main").classList
+const GetAddNote = document.getElementById("addNote").classList
 
 Intro = {  
     toggleHelp(){
         GetHelp.toggle("hidden")
+    }
+}
+
+Notes = {
+    remove(position){
+        document.querySelector("#addNote div:nth-child(" + position + ")").classList.add("hidden")
     }
 }
